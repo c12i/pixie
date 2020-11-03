ae62a198ab3d7cb007fcbc928c8d4a8ec27b7da7
Collins Muriuki <murerwacollins@gmail.com>
2020-11-03 20:08:13 +0300

    Add 8bit number to hexadecimal conversion util

--- utils.js
+++ utils.js
@@ -22,3 +22,14 @@ export function elt(type, props, ...children) {
   }
   return dom;
 }
+
+/**
+ * A hex helper function facilitate conversion of from 8 bit
+ * numerical presentation of color to base-16. To ensure each
+ * number takes two digits we call `padStart` to add a leading zero
+ * when necessary
+ * @param {*} n
+ */
+export function hex(n) {
+  return n.toString(16).padStart(2, "0");
+}
